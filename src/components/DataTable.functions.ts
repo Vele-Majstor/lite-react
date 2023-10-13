814003b626cfeae7e136f53d23b37534bde6e02b
= <hristijan1veleski@gmail.com>
2023-10-13 02:25:13 +0200

    Datatable update

--- src/components/DataTable.functions.ts
+++ src/components/DataTable.functions.ts
@@ -5,5 +5,10 @@ export const sortStates = {
 };
 
 export function createRows(rows: (unknown | object | any)[]) {
-  return rows.map((row) => ({ _id: window.crypto.randomUUID(), ...row }));
+  return rows.map((row) => ({
+    _disabled: false,
+    _selected: false,
+    _id: window.crypto.randomUUID(),
+    ...row,
+  }));
 }
--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -1,11 +1,11 @@
 import {
-    ReactNode,
-    useState,
-    useRef,
-    Dispatch,
-    SetStateAction,
-    ComponentProps,
-    ChangeEvent,
+  ReactNode,
+  useState,
+  useRef,
+  Dispatch,
+  SetStateAction,
+  ChangeEvent,
+  useEffect,
 } from "react";
 import { Size } from "./Table.types";
 import Table from "./Table";
@@ -17,184 +17,214 @@ import TableRow from "./TableRow";
 import TableBody from "./TableBody";
 import TableCell from "./TableCell";
 import TableSelectAll from "./TableSelectAll";
-import Checkbox from "./Checkbox";
 import TableSelectRow from "./TableSelectRow";
 
 type Header = { key: string; header: ReactNode; isSortable?: boolean };
 
 type Props = {
-    selectionMode?: boolean;
-    selection?: (unknown | object | any)[] | null;
-    setSelection?: Dispatch<SetStateAction<(unknown | object | any)[] | null>>;
-    headers: Header[];
-    rows: (unknown | object | any)[] & {
-        _disabled?: boolean;
-        _selected?: boolean;
-    };
-    overflowMenuOnHover?: boolean;
-    size?: Size;
-    stickyHeader?: boolean;
-    staticWidth?: boolean;
-    zebraStyles?: boolean;
-    locale?: string;
+  selectionMode?: boolean;
+  selection?: (unknown | object | any)[] | null;
+  setSelection?: Dispatch<SetStateAction<(unknown | object | any)[] | null>>;
+  headers: Header[];
+  rows: (unknown | object | any)[] & {
+    _disabled?: boolean;
+    _selected?: boolean;
+  };
+  overflowMenuOnHover?: boolean;
+  size?: Size;
+  stickyHeader?: boolean;
+  staticWidth?: boolean;
+  zebraStyles?: boolean;
+  locale?: string;
 };
 
 function getNextSortDirection(
-    header: Header,
-    prevState: { sortDirection: string; header: Header | null }
+  header: Header,
+  prevState: { sortDirection: string; header: Header | null }
 ) {
-    if (prevState.header?.key === header.key) {
-        if (prevState.sortDirection === "NONE") {
-            return { sortDirection: sortStates.ASC, header: prevState.header };
-        }
-        if (prevState.sortDirection === "ASC") {
-            return { sortDirection: sortStates.DESC, header: prevState.header };
-        }
-        return { sortDirection: sortStates.NONE, header: null };
+  if (prevState.header?.key === header.key) {
+    if (prevState.sortDirection === "NONE") {
+      return { sortDirection: sortStates.ASC, header: prevState.header };
     }
-    return { sortDirection: sortStates.ASC, header };
+    if (prevState.sortDirection === "ASC") {
+      return { sortDirection: sortStates.DESC, header: prevState.header };
+    }
+    return { sortDirection: sortStates.NONE, header: null };
+  }
+  return { sortDirection: sortStates.ASC, header };
 }
 
 function DataTable({
-    selectionMode,
-    selection,
-    setSelection,
-    headers,
-    rows,
-    overflowMenuOnHover,
-    size = "lg",
-    stickyHeader,
-    staticWidth,
-    zebraStyles,
-    locale = "en",
+  selectionMode,
+  selection,
+  setSelection,
+  headers,
+  rows,
+  overflowMenuOnHover,
+  size = "lg",
+  stickyHeader,
+  staticWidth,
+  zebraStyles,
+  locale = "en",
 }: Props) {
-    const initialRows = useRef(rows).current;
+  const initialRowsOrder = useRef(createRows(rows));
 
-    const [tableRows, setTableRows] = useState(createRows(rows));
+  const [tableRows, setTableRows] = useState(initialRowsOrder.current);
 
-    const [sort, setSort] = useState<{
-        sortDirection: string;
-        header: Header | null;
-    }>({
-        sortDirection: sortStates.NONE,
-        header: null,
+  const [sort, setSort] = useState<{
+    sortDirection: string;
+    header: Header | null;
+  }>({
+    sortDirection: sortStates.NONE,
+    header: null,
+  });
+
+  function sortByHeader(header: Header) {
+    setSort((prev) => {
+      const sortDirection = getNextSortDirection(header, prev).sortDirection;
+
+      setTableRows((prevRows) => {
+        if (sortDirection === sortStates.NONE) {
+          return initialRowsOrder.current;
+        }
+
+        const rows = JSON.parse(JSON.stringify(prevRows)).sort(
+          (a: any, b: any) =>
+            defaultSortRow(a[header.key], b[header.key], {
+              sortDirection,
+              sortStates,
+              locale,
+            })
+        );
+        return rows;
+      });
+
+      return getNextSortDirection(header, prev);
     });
+  }
 
-    function sortByHeader(header: Header) {
-        setSort((prev) => {
-            const sortDirection = getNextSortDirection(header, prev).sortDirection;
-
-            setTableRows((prevRows) => {
-                if (sortDirection === sortStates.NONE) {
-                    return initialRows;
-                }
-
-                const rows = JSON.parse(JSON.stringify(prevRows)).sort(
-                    (a: any, b: any) =>
-                        defaultSortRow(a[header.key], b[header.key], {
-                            sortDirection,
-                            sortStates,
-                            locale,
-                        })
-                );
-                return rows;
-            });
-
-            return getNextSortDirection(header, prev);
-        });
+  function onSelectAll(event: {
+    checked: boolean;
+    event: ChangeEvent<HTMLInputElement>;
+  }) {
+    if (event.checked) {
+      initialRowsOrder.current = tableRows.map((row) => ({
+        ...row,
+        _selected: true,
+      }));
+      setTableRows((prev) =>
+        prev.map((row) => ({
+          ...row,
+          _selected: true,
+        }))
+      );
+      return;
     }
-
-    function onSelectAll(event: {
-        checked: boolean;
-        event: ChangeEvent<HTMLInputElement>;
-    }) {
-        if (event.checked) {
-            setSelection &&
-                setSelection(tableRows.map((row) => ({ ...row, _selected: true })));
-            return;
-        }
-        setSelection && setSelection(null);
-    }
-
-    function selectRow(row: any & { _selected?: boolean; _disabled: boolean }) {
-        if (row._selected) {
-            setSelection!(
-                (prevSelection) =>
-                    prevSelection?.filter((s) => s._id === row._id) as any
-            );
-            return;
-        }
-        setSelection!((prevSelection) => prevSelection?.concat(row) as any);
-    }
-
-    const selectAllProps = selectionMode
-        ? {
-            checked: tableRows.length > 0 && selection?.length === tableRows.length,
-            indeterminate:
-                tableRows.length > 0 &&
-                selection?.length > 0 &&
-                selection?.length !== tableRows.length,
-            value: selection,
-            onChange: onSelectAll,
-            id: "select-all",
-            name: "select-all",
-        }
-        : {};
-
-    return (
-        <Table
-            zebraStyles={zebraStyles}
-            overflowMenuOnHover={overflowMenuOnHover}
-            size={size}
-            stickyHeader={stickyHeader}
-            staticWidth={staticWidth}
-        >
-            <TableHead>
-                <TableRow>
-                    {selectionMode && (
-                        <TableSelectAll
-                            {...(selectAllProps as ComponentProps<typeof Checkbox>)}
-                        />
-                    )}
-                    {headers.map((header) => (
-                        <TableHeaders
-                            key={header.key}
-                            header={header}
-                            onClick={() => sortByHeader(header)}
-                            sortDirection={sort.sortDirection}
-                            isSortHeader={header.key === sort.header?.key}
-                        >
-                            {header.header}
-                        </TableHeaders>
-                    ))}
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                {tableRows.map((row, index) => (
-                    <TableRow key={index}>
-                        {selectionMode && (
-                            <TableSelectRow
-                                checked={
-                                    selection?.find((sel) => sel._id === row._id) || row._selected
-                                }
-                                value={
-                                    selection?.find((sel) => sel._id === row._id) || row._selected
-                                }
-                                onChange={() => {
-                                    selectRow(row);
-                                }}
-                                disabled={row._disabled}
-                                id={`${row._id} select`}
-                            />
-                        )}
-                        {headers.map((header, index) => (
-                            <TableCell key={index}>{row[header.key]}</TableCell>
-                        ))}
-                    </TableRow>
-                ))}
-            </TableBody>
-        </Table>
+    initialRowsOrder.current = tableRows.map((row) => ({
+      ...row,
+      _selected: false,
+    }));
+    setTableRows((prev) =>
+      prev.map((row) => ({
+        ...row,
+        _selected: false,
+      }))
     );
+  }
+
+  function setSelectedRows(_row: any, row: any, value: boolean) {
+    return _row._id !== row._id ? { ..._row } : { ..._row, _selected: value };
+  }
+
+  function selectRow(row: any & { _selected?: boolean; _disabled: boolean }) {
+    if (row._selected) {
+      initialRowsOrder.current = initialRowsOrder.current.map((_row) =>
+        setSelectedRows(_row, row, false)
+      );
+      setTableRows((prev) =>
+        prev.map((_row) => setSelectedRows(_row, row, false))
+      );
+      return;
+    }
+    initialRowsOrder.current = initialRowsOrder.current.map((_row) =>
+      setSelectedRows(_row, row, true)
+    );
+    setTableRows((prev) =>
+      prev.map((_row) => setSelectedRows(_row, row, true))
+    );
+  }
+
+  useEffect(() => {
+    const selectionArr: any[] = [];
+    tableRows.forEach((row) => {
+      if (row._selected) selectionArr.push(row);
+    });
+    setSelection && setSelection(selectionArr.length > 0 ? selectionArr : null);
+  }, [tableRows, setSelection]);
+
+  return (
+    <Table
+      zebraStyles={zebraStyles}
+      overflowMenuOnHover={overflowMenuOnHover}
+      size={size}
+      stickyHeader={stickyHeader}
+      staticWidth={staticWidth}
+    >
+      <TableHead>
+        <TableRow>
+          {selectionMode && (
+            <TableSelectAll
+              checked={
+                tableRows.length > 0 && selection?.length === tableRows.length
+              }
+              indeterminate={
+                !!tableRows.length &&
+                !!selection?.length &&
+                selection?.length !== tableRows.length
+              }
+              value={String(
+                tableRows.length > 0 && selection?.length === tableRows.length
+              )}
+              onChange={onSelectAll}
+              id="select-all"
+              name="select-all"
+            />
+          )}
+          {headers.map((header) => (
+            <TableHeaders
+              key={header.key}
+              header={header}
+              onClick={() => sortByHeader(header)}
+              sortDirection={sort.sortDirection}
+              isSortHeader={header.key === sort.header?.key}
+            >
+              {header.header}
+            </TableHeaders>
+          ))}
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {tableRows.map((row) => (
+          <TableRow key={row._id}>
+            {selectionMode && (
+              <TableSelectRow
+                checked={row._selected}
+                value={String(row._selected)}
+                onChange={() => {
+                  selectRow(row);
+                }}
+                disabled={row._disabled}
+                id={`${row._id} select`}
+              />
+            )}
+            {headers.map((header) => (
+              <TableCell key={header.key}>{row[header.key]}</TableCell>
+            ))}
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
 }
 
 export default DataTable;
