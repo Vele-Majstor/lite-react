d72b981a6c579ca97ca71747a5374554bb355844
= <hristijan1veleski@gmail.com>
2023-10-16 00:53:47 +0200

    62 version

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import alias from "@rollup/plugin-alias";
 import terser from "@rollup/plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 import packageJson from "./package.json" assert { type: "json" };
 import postcss from "rollup-plugin-postcss";
@@ -24,6 +25,7 @@ export default [
             },
         ],
         plugins: [
+            peerDepsExternal(),
             resolve(),
             commonjs(),
             typescript({ tsconfig: "./tsconfig.json" }),
