7c274a3dc72630575cc22ec697db45b29872f987
= <hristijan1veleski@gmail.com>
2023-10-15 22:05:06 +0200

    Try alias plugin

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import alias from "@rollup/plugin-alias";
 
 import packageJson from "./package.json" assert { type: "json" };
 import postcss from "rollup-plugin-postcss";
@@ -27,6 +28,16 @@ export default [
             typescript({ tsconfig: "./tsconfig.json" }),
 
             postcss(),
+            alias({
+                entries: [
+                    { find: /^@\/(.*)$/, replacement: "src/$1" },
+                    { find: /^@components\/(.*)$/, replacement: "src/components/$1" },
+                    { find: /^@assets\/(.*)$/, replacement: "src/assets/$1" },
+                    { find: /^@utils\/(.*)$/, replacement: "src/utils/$1" },
+                    { find: /^@type-utils\/(.*)$/, replacement: "src/type-utils/$1" },
+                    // Add entries for other aliases as needed...
+                ],
+            }),
         ],
     },
     {
