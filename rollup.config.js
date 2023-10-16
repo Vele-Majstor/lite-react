adfb7b5d470cf306349d1affae382f133c89af1c
= <hristijan1veleski@gmail.com>
2023-10-16 13:29:48 +0200

    Added styles.css to the bundle

--- rollup.config.js
+++ rollup.config.js
@@ -4,55 +4,62 @@ import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import alias from "@rollup/plugin-alias";
 import terser from "@rollup/plugin-terser";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 import packageJson from "./package.json" assert { type: "json" };
 import postcss from "rollup-plugin-postcss";
 
 export default [
-    {
-        input: "src/index.ts",
-        external: [
-            "react",
-            "react-dom",
-            "react-transition-group",
-            "@types/react-transition-group",
-        ],
-        output: [
-            {
-                file: packageJson.main,
-                format: "cjs",
-                sourcemap: true,
-            },
-            {
-                file: packageJson.module,
-                format: "esm",
-                sourcemap: true,
-            },
-        ],
-        plugins: [
-            resolve(),
-            commonjs(),
-            typescript({ tsconfig: "./tsconfig.json" }),
+  {
+    input: "src/index.ts",
+    external: [
+      "react",
+      "react-dom",
+      "react-transition-group",
+      "@types/react-transition-group",
+    ],
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(),
 
-            postcss(),
-            alias({
-                entries: [
-                    { find: /^@\/(.*)$/, replacement: "src/$1" },
-                    { find: /^@components\/(.*)$/, replacement: "src/components/$1" },
-                    { find: /^@assets\/(.*)$/, replacement: "src/assets/$1" },
-                    { find: /^@utils\/(.*)$/, replacement: "src/utils/$1" },
-                    { find: /^@type-utils\/(.*)$/, replacement: "src/type-utils/$1" },
-                ],
-            }),
-            terser(),
+      alias({
+        entries: [
+          { find: /^@\/(.*)$/, replacement: "src/$1" },
+          { find: /^@components\/(.*)$/, replacement: "src/components/$1" },
+          { find: /^@assets\/(.*)$/, replacement: "src/assets/$1" },
+          { find: /^@utils\/(.*)$/, replacement: "src/utils/$1" },
+          { find: /^@type-utils\/(.*)$/, replacement: "src/type-utils/$1" },
         ],
-    },
-    {
-        input: "dist/esm/types/index.d.ts",
-        output: [{ file: "dist/index.d.ts", format: "esm" }],
-        plugins: [dts()],
+      }),
+      terser(),
+    ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
 
-        external: [/\.css$/, /\.scss$/],
+    external: [/\.css$/, /\.scss$/],
+  },
+  {
+    input: "src/styles/_styles.scss",
+    output: {
+      file: "dist/style.css",
+      name: "style",
     },
+    plugins: [postcss()],
+  },
 ];
